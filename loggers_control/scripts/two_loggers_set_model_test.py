9e597e3fe87233a3469e8e514f5558360bd6858c
linzhank <linzhank@gmail.com>
2019-02-17 17:13:59 -0500

    update two loggers model, use round chassis

--- loggers_control/scripts/two_loggers_set_model_test.py
+++ loggers_control/scripts/two_loggers_set_model_test.py
@@ -15,18 +15,17 @@ if __name__ == "__main__":
   reset_world = rospy.ServiceProxy('/gazebo/reset_world', Empty)
   cmd_pub_0 = rospy.Publisher("/cmd_vel_0", Twist, queue_size=1)
   cmd_pub_1 = rospy.Publisher("/cmd_vel_1", Twist, queue_size=1)
-  set_link_state_publisher = rospy.Publisher("/gazebo/set_link_state", LinkState, queue_size=100)
   set_robot_state_publisher = rospy.Publisher("/gazebo/set_model_state", ModelState, queue_size=100)
   rospy.init_node("two_loggers_test" , anonymous=True, log_level=rospy.DEBUG)
   cmd_vel_0 = Twist()
   cmd_vel_1 = Twist()
   num_episodes = 10
-  num_steps = 10
+  num_steps = 100
   rate = rospy.Rate(10)
 
   for ep in range(num_episodes):
     reset_world()
-    mag = random.uniform(0, 1) # robot vector magnitude
+    mag = random.uniform(0, 4) # robot vector magnitude
     ang = random.uniform(-math.pi, math.pi) # robot vector orientation
     x = mag * math.cos(ang)
     y = mag * math.sin(ang)
@@ -44,19 +43,19 @@ if __name__ == "__main__":
     robot_state.pose.orientation.w = w
     # robot_state.reference_frame = "world"
     rate = rospy.Rate(10)
-    # for _ in range(num_steps):
-    set_robot_state_publisher.publish(robot_state)
-    rate.sleep()
-    # for st in range(num_steps):
-    #   lin_0 = random.uniform(-.4,.4)
-    #   ang_0 = random.uniform(-np.pi/6,np.pi/6)
-    #   cmd_vel_0.linear.x = lin_0
-    #   cmd_vel_0.angular.z = ang_0
-    #   lin_1 = random.uniform(-.4,.4)
-    #   ang_1 = random.uniform(-np.pi/6,np.pi/6)
-    #   cmd_vel_1.linear.x = lin_1
-    #   cmd_vel_1.angular.z = ang_1
-    #   rospy.logdebug("step: {}, \ncommand velocity #0: {}, \ncommand velocity #1: {}".format(st, (lin_0,ang_0),(lin_1,ang_1)))
-    #   cmd_pub_0.publish(cmd_vel_0)
-    #   cmd_pub_1.publish(cmd_vel_1)
-    #   rate.sleep()
+    for _ in range(10):
+      set_robot_state_publisher.publish(robot_state)
+      rate.sleep()
+    for st in range(num_steps):
+      lin_0 = random.uniform(0,.2)
+      ang_0 = random.uniform(-np.pi/6,np.pi/6)
+      cmd_vel_0.linear.x = lin_0
+      cmd_vel_0.angular.z = ang_0
+      lin_1 = random.uniform(0,.2)
+      ang_1 = random.uniform(-np.pi/3,np.pi/3)
+      cmd_vel_1.linear.x = lin_1
+      cmd_vel_1.angular.z = ang_1
+      rospy.logdebug("episode: {}, step: {}, \ncommand velocity #0: {}, \ncommand velocity #1: {}".format(ep, st, (lin_0,ang_0),(lin_1,ang_1)))
+      cmd_pub_0.publish(cmd_vel_0)
+      cmd_pub_1.publish(cmd_vel_1)
+      rate.sleep()
