9bc022d5bfbb1e5f4c2401545373e0e06becb865
linzhank <linzhank@gmail.com>
2019-02-28 00:22:30 -0500

    recreate vpg training v0

--- loggers_control/scripts/solo_escape_vpg_train_v0.py
+++ loggers_control/scripts/solo_escape_vpg_train_v0.py
@@ -10,7 +10,7 @@ from __future__ import absolute_import, division, print_function
 
 import sys
 sys.path.insert(0, "/home/linzhank/ros_ws/src/two_loggers/loggers_control/scripts/envs")
-
+import argparse
 import numpy as np
 import tensorflow as tf
 import rospy
@@ -18,24 +18,20 @@ import random
 import os
 import time
 from datetime import datetime
-import pickle
-import csv
-import matplotlib.pyplot as plt
 
 from solo_escape_task_env import SoloEscapeEnv
-from utils import bcolors
+import utils
 
 
-def mlp(x, sizes, activation=tf.tanh, output_activation=None):
-  # Build a feedforward neural network.
-  for size in sizes[:-1]:
-    x = tf.layers.dense(x, units=size, activation=activation)
-  return tf.layers.dense(x, units=sizes[-1], activation=output_activation)
-
-def train(agent, model_path, dim_state=7, num_actions=2, hidden_sizes=[32], learning_rate=1e-3, num_episodes=50, num_steps=64):
+def train(agent, model_path,
+          dim_state=7, num_actions=3,
+          hidden_sizes=[64], learning_rate=1e-3,
+          num_episodes=400, num_steps=1000,
+          bonus_wall=-.01, bonus_door=0.1,
+          time_bonus_func=utils.zero, distance_bonus_func=utils.zero, approach_bonus_func=utils.zero):
   # make core of policy network
   states_ph = tf.placeholder(shape=(None, dim_state), dtype=tf.float32)
-  logits = mlp(states_ph, sizes=hidden_sizes+[num_actions])
+  logits = utils.mlp(states_ph, sizes=hidden_sizes+[num_actions])
   # make action selection op (outputs int actions, sampled from policy)
   actions_id = tf.squeeze(tf.multinomial(logits=logits,num_samples=1), axis=1)
   # make loss function whose gradient, for the right data, is policy gradient
@@ -64,6 +60,7 @@ def train(agent, model_path, dim_state=7, num_actions=2, hidden_sizes=[32], lear
     done = False            # signal from environment that episode is over
     ep_rewards = []            # list for rewards accrued throughout ep
     dist_0 = np.linalg.norm(state[:2]-np.array([0,-6.02]))
+    bonus_distance = distance_bonus_func(1./11, dist_0, num_steps)
     for st in range(num_steps):
       # save obs
       batch_states.append(state.copy())
@@ -76,20 +73,27 @@ def train(agent, model_path, dim_state=7, num_actions=2, hidden_sizes=[32], lear
       else: # forward
         action = np.array([.5, 0.])
       state, rew, done, info = agent.env_step(action)
-      # add small reward if bot getting closer to exit
-      dist = np.linalg.norm(state[:2]-np.array([0,-6.02]))
+      # compute current distance to exit
+      dist = np.linalg.norm(state[:2]-np.array([0,-6.2]))
+      # consider bonus terms
+      bonus_time = time_bonus_func(bonus_wall, num_steps)
+      bonus_approach = approach_bonus_func(dist_0, dist, num_steps)
       # adjust reward based on relative distance to the exit
       if info["status"] == "escaped":
-        rew *= num_steps
+        bonus = bonus_time+bonus_distance
       elif info["status"] == "door":
-        rew += 0.01/(state[1]+6.02)*num_steps
-      else:
-        rew += (dist_0-dist)
+        bonus = bonus_time+bonus_door
+      elif info["status"] == "trapped":
+        bonus = bonus_time+bonus_approach
+      else: # hit wall
+        bonus = bonus_time+bonus_wall
+      rew += bonus
       # save action, reward
       batch_actions.append(action_id)
       ep_rewards.append(rew)
-      # update bot's distance to exit
+      # update robot's distance to exit
       dist_0 = dist
+      # log this step
       rospy.loginfo("Episode: {}, Step: {} \naction: {}, state: {}, reward: {}, done: {}".format(
         ep,
         st,
@@ -106,8 +110,6 @@ def train(agent, model_path, dim_state=7, num_actions=2, hidden_sizes=[32], lear
     batch_lengths.append(ep_length)
     # the weight for each logprob(a|s) is R(tau)
     batch_rtaus += [ep_return] * ep_length
-    # if len(batch_states) > batch_size:
-    #   break
     # take a single policy gradient update step
     batch_loss, _ = sess.run([loss, train_op],
                              feed_dict={
@@ -118,78 +120,87 @@ def train(agent, model_path, dim_state=7, num_actions=2, hidden_sizes=[32], lear
     return batch_loss, batch_returns, batch_lengths
   
   # training loop
-  deposit_returns = []
+  episodic_returns = []
   for ep in range(num_episodes):
     batch_loss, batch_returns, batch_lengths = train_one_episode()
-    print('epoch: %3d \t loss: %.3f \t return: %.3f \t ep_len: %.3f'%
-                (ep, batch_loss, np.mean(batch_returns), np.mean(batch_lengths)))
-    deposit_returns.append(batch_returns)
+    print("epoch: {} \t loss: {} \t return: {}\t ep_len: {}".format(ep, batch_loss, batch_returns[0], np.mean(batch_lengths)))
+    episodic_returns.append(batch_returns[0])
     save_path = saver.save(sess, model_path)
     rospy.loginfo("Model saved in path : {}".format(save_path))
     rospy.logerr("Success Count: {}".format(agent.success_count))
   # plot returns
-  fig, ax = plt.subplots()
-  ax.plot(np.arange(len(deposit_returns)), deposit_returns)
-  ax.set(xlabel="Time Step", ylabel='Episodic Return')
-  ax.grid()
-  figure_fname = os.path.join(os.path.dirname(model_path),"returns.png")
-  plt.savefig(figure_fname)
-  plt.close(fig)
-  
+  utils.plot_returns(returns=episodic_returns, mode=2, save_flag=True, path=model_path)  
 
 if __name__ == "__main__":
+  # make arg parser
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--model_path", type=str,
+                      default="/home/linzhank/ros_ws/src/two_loggers/loggers_control/vpg_model-"+datetime.now().strftime("%Y-%m-%d-%H-%M")+"/model.ckpt")
+  parser.add_argument("--hidden_sizes", type=int, default = 64)
+  parser.add_argument("--learning_rate", type=float, default = 1e-3)
+  parser.add_argument("--num_episodes", type=int, default = 400)
+  parser.add_argument("--num_steps", type=int, default = 1000)
+  parser.add_argument("--bonus_wall", type=float, default = -.01)
+  parser.add_argument("--bonus_door", type=float, default = .1)
+  parser.add_argument("--bonus_time", type=bool, default=False)
+  parser.add_argument("--bonus_distance", type=bool, default=False)
+  parser.add_argument("--bonus_approach", type=bool, default=False)
+  args = parser.parse_args()
+
+  # Main really starts here
   start_time = time.time()
   rospy.init_node("solo_escape_vpg", anonymous=True, log_level=rospy.INFO)
   # make an instance from env class
   escaper = SoloEscapeEnv()
-  # make hyper-parameters
   statespace_dim = 7 # x, y, x_dot, y_dot, cos_theta, sin_theta, theta_dot
   actionspace_dim = 2
-  hidden_sizes = [64]
-  learning_rate = 1e-3
-  num_episodes = 300
-  num_steps = 1000
-  model_path = "/home/linzhank/ros_ws/src/two_loggers/loggers_control/vpg_model-" +\
-                 datetime.now().strftime("%Y-%m-%d-%H-%M")+"/model.ckpt"
   # store hyper-parameters
+  time_bonus_func = utils.zero
+  distance_bonus_func = utils.zero
+  approach_bonus_func = utils.zero
+  if args.bonus_time:
+    time_bonus_func = utils.wallBonusDividedNumsteps
+  if args.bonus_distance:
+    distance_bonus_func = utils.weightedD0
+  if args.bonus_approach:
+    approach_bonus_func = utils.d0MinusD
+  # make core of policy network
+  train(agent=escaper, model_path=args.model_path,
+        dim_state = statespace_dim, num_actions=actionspace_dim,
+        hidden_sizes=[args.hidden_sizes], learning_rate=args.learning_rate,
+        num_episodes=args.num_episodes, num_steps=args.num_steps,
+        bonus_wall=args.bonus_wall, bonus_door=args.bonus_door,
+        time_bonus_func=time_bonus_func, distance_bonus_func=distance_bonus_func, approach_bonus_func=approach_bonus_func)
+  # time
+  end_time = time.time()
+  training_time = end_time - start_time
+
+  # Main actually ends here
+  # store hyper parameters
   hyp_params = {
     "statespace_dim": statespace_dim,
     "actionspace_dim": actionspace_dim,
-    "hidden_sizes": hidden_sizes,
-    "learning_rate": learning_rate,
-    "num_episodes": num_episodes,
-    "num_steps": num_steps,
+    "hidden_sizes": args.hidden_sizes,
+    "learning_rate": args.learning_rate,
+    "num_episodes": args.num_episodes,
+    "num_steps": args.num_steps,
   }               
-  # make core of policy network
-  train(agent=escaper, model_path=model_path, dim_state = statespace_dim,
-        num_actions=actionspace_dim, hidden_sizes=hidden_sizes,
-        learning_rate=learning_rate, num_episodes=num_episodes,
-        num_steps=num_steps)
-  rospy.logdebug("success: {}".format(escaper.success_count))
-  end_time = time.time()
-  training_time = end_time - start_time
-  # store results
-  results = {
-    "success_count": escaper.success_count,
-    "training_time": training_time,
-    "statespace_dim": statespace_dim,
-    "actionspace_dim": actionspace_dim,
-    "hidden_sizes": hidden_sizes,
-    "learning_rate": learning_rate,
-    "num_episodes": num_episodes,
-    "num_steps": num_steps
-  }
+  # store training information
+  train_info = hyp_params
+  train_info["success_count"] = escaper.success_count
+  train_info["training_time"] = training_time
+  train_info["bonus_wall"] = args.bonus_wall
+  train_info["bonus_door"] = args.bonus_door
+  train_info["bonus_time"] = time_bonus_func.func_name
+  train_info["bonus_distance"] = distance_bonus_func.func_name
+  train_info["bonus_approach"] = approach_bonus_func.func_name
+
   # save hyper-parameters
-  file_name = "hyper_parameters.pkl"
-  file_dir = os.path.dirname(model_path)
-  file_path = os.path.join(file_dir,file_name)
-  with open(file_path, "wb") as hfile:
-    pickle.dump(hyp_params, hfile, pickle.HIGHEST_PROTOCOL)
+  utils.save_pkl(fname="hyper_parameters.pkl",
+             path=args.model_path,
+             content=hyp_params)
   # save results
-  file_name = "results.csv"
-  file_dir = os.path.dirname(model_path)
-  file_path = os.path.join(file_dir,file_name)
-  with open(file_path, "w") as rfile:
-    for key in results.keys():
-      rfile.write("{},{}\n".format(key,results[key]))
+  utils.save_csv(fname="results.csv",
+                 path=args.model_path,
+                 content=train_info)
   
--- loggers_control/scripts/utils.py
+++ loggers_control/scripts/utils.py
@@ -1,3 +1,17 @@
+#! /usr/bin/env python
+"""
+Useful class and functions for loggers tasks
+"""
+import numpy as np
+import csv
+import pickle
+import tensorflow as tf
+import os
+from datetime import datetime
+import pickle
+import csv
+import matplotlib.pyplot as plt
+
 class bcolors:
     """ For the purpose of print in terminal with colors """
     HEADER = '\033[95m'
@@ -10,8 +24,80 @@ class bcolors:
     UNDERLINE = '\033[4m'
     
 # bonus functions
-def bonusWallDividedNumsteps(bonus_wall, num_steps): return bonus_wall/num_steps # bonus for every time step
-def weightedD0(weight,d0): return weight*d0 # bonus for initial distance
-def d0MinusD(d0,d,num_stes): return (d0-d)/num_steps # bonus of approaching the exit
+def wallBonusDividedNumsteps(bonus_wall, num_steps): return bonus_wall/num_steps # bonus for every time step
+def weightedD0(weight,d0,amplifier): return weight*d0*amplifier # bonus for initial distance
+def d0MinusD(d0,d,num_steps): return (d0-d)/num_steps # bonus of approaching the exit
 def zero(x,y,z=0): return 0
 
+def mlp(x, sizes, activation=tf.tanh, output_activation=None):
+  # Build a feedforward neural network.
+  for size in sizes[:-1]:
+    x = tf.layers.dense(x, units=size, activation=activation)
+  return tf.layers.dense(x, units=sizes[-1], activation=output_activation)
+
+# save pickle
+def save_pkl(fname, path, content):
+  """
+  Save content into path/name as pickle file
+  Args:
+    fname: file name, str
+    path: file path, str
+    content: to be saved, array/dict/list...
+  """
+  fdir = os.path.dirname(path)
+  file_path = os.path.join(fdir,fname)
+  with open(file_path, "wb") as f:
+    pickle.dump(content, f, pickle.HIGHEST_PROTOCOL)
+
+def save_csv(fname, path, content):
+  """
+  Save content into path/name as csv file
+  Args:
+    fname: file name, str
+    path: file path, str
+    content: to be saved, dict
+  """
+  fdir = os.path.dirname(model_path)
+  file_path = os.path.join(fdir,fname)
+  with open(file_path, "w") as f:
+    for key in content.keys():
+      f.write("{},{}\n".format(key,content[key]))
+
+def plot_returns(returns, mode, save_flag, path):
+  """
+  Plot rewards
+  Args:
+    returns: episodic returns, list
+    mode: 0 - returns; 1 - accumulated returns; 2 - averaged returns, int
+    save_flag: save figure to file or not, bool
+    path: file path, str
+  """
+  assert mode==0 or mode==1 or mode==2
+  acc_returns = []
+  ave_returns = []
+  acc_r = returns[0]
+  ave_r = acc_r
+  for i,r in enumerate(returns):
+    acc_r += r
+    ave_r = acc_r/(i+1)
+    acc_returns.append(acc_r)
+    ave_returns.append(ave_r)
+  fig, ax = plt.subplots()
+  if mode == 0:
+    ax.plot(np.arange(len(returns)), returns)
+    ax.set(xlabel="Episode", ylabel="Returns")
+    figure_dir = os.path.join(os.path.dirname(path),"episodic_returns.png")
+  elif mode == 1:
+    ax.plot(np.arange(len(acc_returns)), acc_returns)
+    ax.set(xlabel="Episode", ylabel="Accumulated Returns")
+    figure_dir = os.path.join(os.path.dirname(path),"accumulated_returns.png")
+  else:
+    ax.plot(np.arange(len(ave_returns)), ave_returns)
+    ax.set(xlabel="Episode", ylabel="Averaged Returns")
+    figure_dir = os.path.join(os.path.dirname(path),"averaged_returns.png")
+  ax.grid()
+  if save_flag:
+    plt.savefig(figure_dir)
+  else:
+    plt.show()
+  plt.close(fig)
