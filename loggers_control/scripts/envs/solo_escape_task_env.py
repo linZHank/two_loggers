7801e62cd445d595577804b3d42937978ec8d199
linzhank <linzhank@gmail.com>
2019-03-03 18:00:11 -0500

    mod south wall condition

--- loggers_control/scripts/envs/solo_escape_task_env.py
+++ loggers_control/scripts/envs/solo_escape_task_env.py
@@ -213,12 +213,12 @@ class SoloEscapeEnv(object):
       self.status = "north"
       self._episode_done = True
       rospy.logwarn("Logger is too close to north wall!")
-    elif -4.82<self.curr_pose[1]<-4.8 and np.absolute(self.curr_pose[0])>1 :
+    elif self.curr_pose[1]<=-4.8 and np.absolute(self.curr_pose[0])>1 :
       reward = -0.
       self.status = "south"
       self._episode_done = True
       rospy.logwarn("Logger is too close to south wall!")
-    elif -6<self.curr_pose[1]<=-4.82 and np.absolute(self.curr_pose[0])>0.8:
+    elif -6<self.curr_pose[1]<-4.8 and np.absolute(self.curr_pose[0])>0.8:
       reward = 0.
       self.status = "door"
       self._episode_done = True
