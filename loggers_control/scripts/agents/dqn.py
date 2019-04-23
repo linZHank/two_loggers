73af7d7960921dc9fff61a675a7af9fca9455130
linzhank <linzhank@gmail.com>
2019-04-23 10:58:13 -0400

    use sequential model instead of advanced model

--- loggers_control/scripts/agents/dqn.py
+++ loggers_control/scripts/agents/dqn.py
@@ -1,20 +1,28 @@
 from __future__ import absolute_import, division, print_function
 
+import numpy as np
 import ag_utils
 from tensorflow.keras.layers import Dense
 from tensorflow.keras import Model
 
-class QNet(Model):
-    def __init__(self, num_actions):
-        super(Model,self).__init__()
-        self.hidden1 = Dense(128, activation="relu")
-        self.hidden2 = Dense(128, activation="relu")
-        self.output= Dense(num_actions)
 
-    def call(self, state):
-        feature = self.hidden1(state)
-        feature = self.hidden2(feature)
-        return self.output(feature)
+class Memory:
+    def __init__(self, memory_cap):
+        self.memory_cap = memory_cap
+        self.memory = []
+    def store(self, experience):
+        # pop a random experience if memory full
+        if len(self.memory) >= self.memory_cap:
+            self.memory.pop(np.random.randint(len(self.memory))
+        self.memory.append(experience)
+    def sample_batch(self, batch_size):
+        # Select batch
+        if len(self.memory) < batch_size:
+            batch = random.sample(self.memory, len(self.memory))
+        else:
+            batch = random.sample(self.memory, batch_size)
+
+        return zip(*batch)
 
 class DQNAgent:
     def __init__(self):
@@ -22,17 +30,31 @@ class DQNAgent:
 
     def train(self, env, actions, dim_state, num_actions,
               batch_size=1000, num_episodes=512, num_steps=1000):
-        states = mp.empty((batch_size, dim_state))
+        replay_memory = Memory(memory_cap=50000) # init replay memory
+        qnet_active = tf.keras.models.Sequential([
+            tf.keras.layers.Dense(128, input_shape=(7, ), activation='relu'),
+            tf.keras.layers.Dense(128, activation='relu'),
+            tf.keras.layers.Dense(num_actions, activation='softmax')
+        ]) # init Q(s,a)
+        qnet_stable = tf.keras.models.Sequential([
+            tf.keras.layers.Dense(128, input_shape=(7, ), activation='relu'),
+            tf.keras.layers.Dense(128, activation='relu'),
+            tf.keras.layers.Dense(num_actions, activation='softmax')
+        ]) # init Q^(s,a)
+        states = np.empty((batch_size, dim_state))
         actions = np.empty((batch_size,), dtype=np.int32)
+        update_counter = 10000
         for ep in range(num_episodes):
             obs, _ = env.reset()
             state_0 = utils.obs_to_state(obs)
             done, ep_rewards = False, []
             for st in range(num_steps):
-                action = utils.epsilon_greedy(self.qnet(state_0), epsilon)
+                action =  utils.epsilon_greedy(qnet_active, state_0, epsilon)
                 obs, rew, done, info = env.step(action)
                 state_1 = utils.obs_to_state(obs)
-                utils.store_experience(state_0, action, rew, done, state_1, replay_memory)
+                replay_memory.store((state_0, action, rew, done, state_1))
+                sample = replay_memory.sample_batch(batch_size)
+                # create dataset
                 utils.set_q_target(utils.sample_batch(replay_memory, batch_size), self.qnet_hat)
                 utils.train_batch
                 if update_weights:
--- loggers_control/scripts/agents/qnet_test.py
+++ loggers_control/scripts/agents/qnet_test.py
@@ -1,11 +1,25 @@
 from __future__ import absolute_import, division, print_function
 
+import numpy as np
 import tensorflow as tf
-import from dqn import QNet
+from dqn import QNet
 
-qnet = QNet()
-loss_object = tf.keras.losses.SparseCategoricalCrossentropy()
-optimizer = tf.keras.optimizers.Adam()
-
-train_loss = tf.keras.metrics.Mean(name='train_loss')
-train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
+x = np.random.randn(10000,7)
+y = np.random.randint(4, size=10000)
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Dense(128, input_shape=(7, ), activation='relu'),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+model_hat = tf.keras.models.Sequential([
+    tf.keras.layers.Dense(128, input_shape=(7, ), activation='relu'),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+model.fit(x, y, epochs=100)
+x_pred = np.random.randn(16,7)
+preds = np.argmax(model.predict(x_pred), axis=1)
+preds_hat = np.argmax(model_hat.predict(x_pred), axis=1)
