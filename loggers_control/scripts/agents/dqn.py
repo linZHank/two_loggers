ba356a18c1765885e0e1c43796563e2e2d0c08fd
linzhank <linzhank@gmail.com>
2019-04-24 11:54:33 -0400

    finish dqn prototype

--- loggers_control/scripts/agents/dqn.py
+++ loggers_control/scripts/agents/dqn.py
@@ -1,9 +1,13 @@
 from __future__ import absolute_import, division, print_function
 
 import numpy as np
-import ag_utils
+import random
+import tensorflow as tf
+from utils import env_utils
 from tensorflow.keras.layers import Dense
 from tensorflow.keras import Model
+
+import pdb
 
 
 class Memory:
@@ -13,8 +17,9 @@ class Memory:
     def store(self, experience):
         # pop a random experience if memory full
         if len(self.memory) >= self.memory_cap:
-            self.memory.pop(np.random.randint(len(self.memory))
+            self.memory.pop(random.randint(0, len(self.memory)-1))
         self.memory.append(experience)
+
     def sample_batch(self, batch_size):
         # Select batch
         if len(self.memory) < batch_size:
@@ -26,25 +31,29 @@ class Memory:
 
 class DQNAgent:
     def __init__(self, hyp_params):
-        super(DQNAgent, self).__init__()
+        # super(DQNAgent, self).__init__()
         # hyper-parameters
         self.epsilon = hyp_params["epsilon"]
         self.actions = hyp_params["actions"]
         self.gamma = hyp_params["gamma"]
+        self.dim_state = hyp_params["dim_state"]
+        self.num_episodes = hyp_params["num_episodes"]
+        self.num_steps = hyp_params["num_steps"]
+        self.batch_size = hyp_params["batch_size"]
         # Q(s,a;theta)
         self.qnet_active = tf.keras.models.Sequential([
-            tf.keras.layers.Dense(128, input_shape=(dim_state, ), activation='relu'),
+            tf.keras.layers.Dense(128, input_shape=(self.dim_state, ), activation='relu'),
             tf.keras.layers.Dense(128, activation='relu'),
-            tf.keras.layers.Dense(num_actions, activation='softmax')
+            tf.keras.layers.Dense(len(self.actions), activation='softmax')
         ])
         self.qnet_active.compile(optimizer="adam",
                             loss="sparse_categorical_crossentropy",
                             metrics=["accuracy"])
         # Q^(s,a;theta_)
         self.qnet_stable = tf.keras.models.Sequential([
-            tf.keras.layers.Dense(128, input_shape=(dim_state, ), activation='relu'),
+            tf.keras.layers.Dense(128, input_shape=(self.dim_state, ), activation='relu'),
             tf.keras.layers.Dense(128, activation='relu'),
-            tf.keras.layers.Dense(num_actions, activation='softmax')
+            tf.keras.layers.Dense(len(self.actions), activation='softmax')
         ])
         # init replay memory
         self.replay_memory = Memory(memory_cap=50000)
@@ -55,7 +64,7 @@ class DQNAgent:
         Else, return a random index
         """
         if np.random.rand() > self.epsilon:
-            return np.argmax(self.qnet_active.predict(state.reshape(1,-1))
+            return np.argmax(self.qnet_active.predict(state.reshape(1,-1)))
         else:
             return np.random.randint(len(self.actions))
 
@@ -63,30 +72,35 @@ class DQNAgent:
         return 1./(epi + 20)
 
     def compute_target_q(self, sampled_batch):
-        target_q = np.array(sampled_batch[1]) + self.gamma *  np.max(self.qnet_stable.predict(np.array(sampled_batch[-1])), axis=1)
-
-
+        present_value = self.qnet_active.predict(np.array(sampled_batch[0]))
+        future_value = self.qnet_stable.predict(np.array(sampled_batch[-1]))
+        target_q = present_value
+        for i, s in enumerate(sampled_batch[3]):
+            if sampled_batch[3][i]:
+                target_q[i,sampled_batch[1]] = sampled_batch[2]
+            else:
+                target_q[i,sampled_batch[1][i]] = sampled_batch[2][i] + self.gamma * np.max(future_value[i])
 
     def train(self, env):
         update_step = 1000
         total_step = 0
-        for ep in range(num_episodes):
+        for ep in range(self.num_episodes):
             self.epsilon = self.epsilon_decay(ep)
             obs, _ = env.reset()
-            state_0 = ag_utils.obs_to_state(obs)
+            state_0 = env_utils.obs_to_state(obs)
             done, ep_rewards = False, []
-            for st in range(num_steps):
+            for st in range(self.num_steps):
                 act_id = self.epsilon_greedy(state_0)
-                action = actions[act_id]
+                action = self.actions[act_id]
                 obs, rew, done, info = env.step(action)
-                state_1 = ag_utils.obs_to_state(obs)
-                replay_memory.store((state_0, act_id, rew, done, state_1))
-                minibatch = replay_memory.sample_batch(batch_size)
+                state_1 = env_utils.obs_to_state(obs)
+                self.replay_memory.store((state_0, act_id, rew, done, state_1))
+                minibatch = self.replay_memory.sample_batch(self.batch_size)
                 # create dataset
                 x = np.array(minibatch[0])
                 y = self.compute_target_q(minibatch)
                 total_step += 1
                 if total_step % update_step == 0:
-                    self.qnet_stable.set_weights(qnet_active.get_weights())
+                    self.qnet_stable.set_weights(self.qnet_active.get_weights())
                 if done:
                     break
--- loggers_control/scripts/solo_escape_dqn.py
+++ loggers_control/scripts/solo_escape_dqn.py
@@ -5,6 +5,7 @@ import sys
 import os
 import numpy as np
 import tensorflow as tf
+import rospy
 
 from envs.solo_escape_task_env import SoloEscapeEnv
 from utils import gen_utils, solo_utils, tf_utils
@@ -13,20 +14,21 @@ from agents.dqn import DQNAgent
 
 if __name__ == "__main__":
     # Main really starts here
-    start_time = time.time()
+    # start_time = time.time()
     rospy.init_node("solo_escape_dqn", anonymous=True, log_level=rospy.INFO)
     # make an instance from env class
     env = SoloEscapeEnv()
-    obs, _ = env.reset()
+    env.reset()
     # hyper-parameters
+    hyp_params = {}
     hyp_params["dim_state"] = len(solo_utils.obs_to_state(env.observation))
     hyp_params["actions"] = np.array([np.array([.5, -1]), np.array([.5, 1])])
-    hyp_params["num_episodes"] = 500
-    hyp_params["num_steps"] = 512
+    hyp_params["num_episodes"] = 400
+    hyp_params["num_steps"] = 200
+    hyp_params["batch_size"] = 100
     hyp_params["epsilon"] = 0.2
     hyp_params["gamma"] = 0.99
-
-
+    # instantiate agent
     agent = DQNAgent(hyp_params)
     agent.train(env)
 
