be828ccd5ecd71b2c11e62bdb969c50345522b8a
linzhank <linzhank@gmail.com>
2019-12-29 15:55:12 -0500

    save new models for narrowed opening

--- loggers_control/scripts/double_escape_dqn_eval.py
+++ loggers_control/scripts/double_escape_dqn_eval.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     env = DoubleEscapeEnv()
     env.reset()
     # load agent models
-    model_dir = os.path.dirname(sys.path[0])+"/saved_models/double_escape/dqn/2019-12-24-16-08/"
+    model_dir = os.path.dirname(sys.path[0])+"/saved_models/double_escape/dqn/2019-12-27-15-07/"
     with open(os.path.join(model_dir,"agent_0/agent_parameters.pkl"), "rb") as f:
         agent_params_0 = pickle.load(f)
     with open(os.path.join(model_dir,"agent_1/agent_parameters.pkl"), "rb") as f:
