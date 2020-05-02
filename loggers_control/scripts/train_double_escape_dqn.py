78e196daf9986b88c0ed69bc6aa5f47ae1bc50c9
linzhank <linzhank@gmail.com>
2020-05-02 00:52:57 -0400

    prototype decentralized dqn training for double_escape

--- loggers_control/scripts/train_double_escape_dqn.py
+++ loggers_control/scripts/train_double_escape_dqn.py
@@ -16,19 +16,20 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 import rospy
 
-from envs.solo_escape_discrete_env import SoloEscapeDiscreteEnv
+from envs.double_escape_discrete_env import DoubleEscapeDiscreteEnv
 from agents.dqn import DQNAgent
 
 
 if __name__ == "__main__":
-    env=SoloEscapeDiscreteEnv()
-    agent = DQNAgent(env=env, name='dqn_logger')
+    env=DoubleEscapeDiscreteEnv()
+    agent0 = DQNAgent(env=env, name='dqn_logger0', layer_sizes=[128,128], warmup_episodes=10)
+    agent1 = DQNAgent(env=env, name='dqn_logger1', layer_sizes=[128,128], warmup_episodes=10)
     date_time = datetime.now().strftime("%Y-%m-%d-%H-%M")
-    model_dir = sys.path[0]+"/saved_models/solo_escape/dqn/"+date_time
-    num_episodes = 8000
+    num_episodes = 100
     num_steps = env.max_steps
-    num_samples = 1 # sample k times to train q-net
-    episodic_returns, sedimentary_returns = [], []
+    num_samples0, num_samples1 = 1, 1 # sample k times to train q-net
+    episodic_returns0, episodic_returns1 = [], []
+    sedimentary_returns0, sedimentary_returns1 = [], []
     step_counter = 0
     success_counter = 0
     start_time = time.time()
@@ -37,29 +38,38 @@ if __name__ == "__main__":
         rewards = []
         # reset env and get state from it
         obs = env.reset()
-        agent.linear_epsilon_decay(episode=ep, decay_period=512)
+        agent0.linear_epsilon_decay(episode=ep, decay_period=12)
+        agent0.linear_epsilon_decay(episode=ep, decay_period=12)
         for st in range(num_steps):
             # take actions, no action will take if deactivated
-            act = agent.epsilon_greedy(obs)
+            act0 = agent0.epsilon_greedy(obs)
+            act1 = agent1.epsilon_greedy(obs)
+            act = int(act0*5+act1)
             # step env
             next_obs, rew, done, info = env.step(act)
             # store transitions and train
-            agent.replay_memory.store([obs.copy(), act, rew, done, next_obs])
-            if ep >= agent.warmup_episodes:
-                for _ in range(num_samples):
-                    agent.train()
-                step_counter += 1
-            rewards.append(rew)
-            if info == "escaped":
-                success_counter += 1
+            agent0.replay_memory.store([obs.copy(), act, rew, done, next_obs])
+            agent1.replay_memory.store([obs.copy(), act, rew, done, next_obs])
+            # train agent0
+            if ep >= agent0.warmup_episodes:
+                for _ in range(num_samples0):
+                    agent0.train()
+            # train agent1
+            if ep >= agent1.warmup_episodes:
+                for _ in range(num_samples0):
+                    agent0.train()
             # log step
-            rospy.logdebug("\n-\nepisode: {}, step: {}, epsilon: {} \nstate: {} \naction: {} \nnext_state: {} \nreward: {} \ndone: {} \ninfo: {} \nsucceed: {}\n-\n".format(ep+1, st+1, agent.epsilon, obs, act, next_obs, rew, done, info,success_counter))
+            step_counter += 1
+            rewards.append(rew)
+            if done and info.count('escaped')==2:
+                success_counter += 1
+            rospy.logdebug("\n-\nepisode: {}, step: {}, epsilon0: {}, epsilon1: {} \nstate: {} \naction: {} \nnext_state: {} \nreward: {} \ndone: {} \ninfo: {} \nsucceed: {}\n-\n".format(ep+1, st+1, agent0.epsilon, agent1.epsilon, obs, act, next_obs, rew, done, info, success_counter))
             obs = next_obs.copy()
             if done:
                 # summarize episode
                 episodic_returns.append(sum(rewards))
                 sedimentary_returns.append(sum(episodic_returns)/(ep+1))
-                rospy.loginfo("\n================================================================\nEpisode: {} \nSteps: {} \nEpsilon: {} \nEpisodicReturn: {} \nAveragedReturn: {} \nEndState: {} \nTotalSuccess: {} \nTimeElapsed: {} \n================================================================n".format(ep+1, st+1, agent.epsilon, episodic_returns[-1], sedimentary_returns[-1], info, success_counter, time.time()-start_time))
+                rospy.loginfo("\n================================================================\nEpisode: {} \nSteps: {} \nEpsilon0: {} \nEpsilon1: {} \nEpisodicReturn: {} \nAveragedReturn: {} \nEndState: {} \nTotalSuccess: {} \nTimeElapsed: {} \n================================================================n".format(ep+1, st+1, agent0.epsilon, agent1.epsilon, episodic_returns[-1], sedimentary_returns[-1], info, success_counter, time.time()-start_time))
                 break
 
     # plot averaged returns
@@ -68,7 +78,10 @@ if __name__ == "__main__":
     ax.plot(sedimentary_returns)
     plt.show()
     # save model
-    agent.save_model()
-    agent.save_params()
+    agent0.save_model()
+    agent0.save_params()
+    agent1.save_model()
+    agent1.save_params()
     # save returns
-    np.save(os.path.join(agent.model_dir, 'ep_returns.npy'), episodic_returns)
+    np.save(os.path.join(agent0.model_dir, 'ep_returns.npy'), episodic_returns)
+    np.save(os.path.join(agent1.model_dir, 'ep_returns.npy'), episodic_returns)
