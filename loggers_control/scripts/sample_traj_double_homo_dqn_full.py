23ea00888562731524dd9a9f26e22a7d1658c42b
linzhank <linzhank@gmail.com>
2020-06-29 02:18:09 -0400

    create traj samples

--- loggers_control/scripts/sample_traj_double_homo_dqn_full.py
+++ loggers_control/scripts/sample_traj_double_homo_dqn_full.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 """
-Sample trajectories with DQN model, full observations
+Sample trajectories with DQN model. Unfortunately, robots orientation cannot be set.
 """
 from __future__ import absolute_import, division, print_function
 
@@ -21,42 +21,62 @@ from envs.double_escape_discrete_env import DoubleEscapeDiscreteEnv
 from agents.dqn import DQNAgent
 
 
-def _extract_yaw(orientation):
-    quat = np.zeros(4)
-    quat[0] =  orientation.x
-    quat[1] =  orientation.y
-    quat[2] =  orientation.z
-    quat[3] =  orientation.w
-    euler = tf.transformations.euler_from_quaternion(quat)
-    yaw = euler[2]
-
-    return yaw
-    
 if __name__ == "__main__":
     env=DoubleEscapeDiscreteEnv()
-    agent = DQNAgent(env=env, name='double_logger_eval')
+    agent = DQNAgent(env=env, name='double_logger_sample')
     model_path = os.path.join(sys.path[0], 'saved_models/double_escape_discrete/dqn/2020-05-29-17-33/double_logger/models/5093500.h5')
     agent.load_model(model_path=model_path)
     agent.epsilon = 0.
     num_steps = env.max_steps
+    traj = []
+    traj_path = os.path.join(sys.path[0], 'saved_trajectories', datetime.now().strftime("%Y-%m-%d-%H-%M"), 'traj.npy')
     # Set double_logger to specified pose
-    init_pose = np.array([0,0,0,pi/4,-pi/4]) # modify this to any pose as needed: [x, y, theta, th0, th1]
-    env.reset()
-    # Set double_logger pose
+    init_pose = np.array([-4,3,pi/4]) # modify this to any pose as needed: [x, y, theta]
+    env.pausePhysics()
+    env.resetWorld()
     double_logger_pose = ModelState()
     double_logger_pose.model_name = "double_logger"
     double_logger_pose.pose.position.z = 0.09
     x = init_pose[0]
     y = init_pose[1]
     theta = init_pose[2]
-    th0 = init_pose[3]
-    th1 = init_pose[4]
     quat = tf.transformations.quaternion_from_euler(0, 0, theta)
-    # q0 = tf.transformations.quaternion_from_euler(0, 0, th0)
-    # q1 = tf.transformations.quaternion_from_euler(0, 0, th1)
     double_logger_pose.pose.position.x = x
     double_logger_pose.pose.position.y = y
     double_logger_pose.pose.orientation.z = quat[2]
     double_logger_pose.pose.orientation.w = quat[3]
     env.setModelState(model_state=double_logger_pose)
+    env.unpausePhysics()
+    for _ in range(15): # zero cmd_vel for another 0.025 sec. Important! Or wrong obs
+        env.cmd_vel0_pub.publish(Twist())
+        env.cmd_vel1_pub.publish(Twist())
+        env.rate.sleep()
+    env.pausePhysics()
 
+    # sampling trajectory
+    obs = env._get_observation()
+    # obs = env.reset()
+    state_0 = obs.copy()
+    state_1 = obs.copy()
+    state_1[:6] = state_1[-6:]
+    for st in range(num_steps):
+        traj.append(obs)
+        act0 = agent.epsilon_greedy(state_0)
+        act1 = agent.epsilon_greedy(state_1)
+        act = np.array([act0, act1])
+        # step env
+        next_obs, rew, done, info = env.step(act)
+        if 'blown' in info:
+            break
+        obs = next_obs.copy()
+        state_0 = obs.copy() # + 0.5*random.randn(obs.shape[0])
+        state_1 = obs.copy() # + 0.5*random.randn(obs.shape[0])
+        state_1[:6] = state_1[-6:]
+        if done:
+            break
+    # save traj
+    traj.append(obs)
+    traj = np.squeeze(np.array([traj]))
+    if not os.path.exists(os.path.dirname(traj_path)):
+        os.makedirs(os.path.dirname(traj_path))
+    np.save(traj_path, traj)
