fc418f07d4101546597ddecb100c206e656890f9
linzhank <linzhank@gmail.com>
2020-07-02 23:14:58 -0400

    compute qval mae when eval double escape

--- loggers_control/scripts/eval_double_hete_dqn_full.py
+++ loggers_control/scripts/eval_double_hete_dqn_full.py
@@ -34,8 +34,10 @@ if __name__ == "__main__":
     episode_counter = 0
     step_counter = 0
     success_counter = 0
+    episodic_qvals_mae = np.zeros(num_episodes)
     while episode_counter<num_episodes:
         start_time = time.time()
+        qvals_diff = []
         # reset env and get state from it
         obs, rewards, done = env.reset(), [], False
         # next 3 lines generate state based on
@@ -48,6 +50,9 @@ if __name__ == "__main__":
             # take actions, no action will take if deactivated
             act0 = agent0.epsilon_greedy(state_0)
             act1 = agent1.epsilon_greedy(state_1)
+            qval0 = np.max(agent0.qnet_active(np.expand_dims(state_0,axis=0)))
+            qval1 = np.max(agent1.qnet_active(np.expand_dims(state_1,axis=0)))
+            qvals_diff.append(np.absolute(qval0-qval1))
             act = np.array([act0, act1])
             # step env
             next_obs, rew, done, info = env.step(act)
@@ -64,11 +69,14 @@ if __name__ == "__main__":
                 success_counter += 1
             rospy.logdebug("\n-\nepisode: {}, step: {} \nstate: {} \naction: {} \nnext_state: {} \nreward: {} \ndone: {} \ninfo: {} \nsucceed: {}\n-\n".format(episode_counter+1, st+1, obs, act, next_obs, rew, done, info, success_counter))
             if done:
-                episode_counter += 1
-                # summarize episode
+                episodic_qvals_mae[episode_counter] = sum(qvals_diff)/len(qvals_diff)
                 episodic_returns.append(sum(rewards))
-                rospy.loginfo("\n================================================================\nEpisode: {} \nSteps: {} \nEpisodicReturn: {} \nEndState: {} \nTotalSuccess: {} \nTimeConsumed: {} \n================================================================n".format(episode_counter, st+1, episodic_returns[-1], info, success_counter, time.time()-start_time))
+                episode_counter += 1
+                rospy.loginfo("\n================================================================\nEpisode: {} \nSteps: {} \nEpisodicReturn: {} \nEndState: {} \nTotalSuccess: {} \nTimeConsumed: {} \n================================================================n".format(episode_counter, st+1, episodic_returns[-1], info, success_counter, sum(qvals_diff)/len(qvals_diff)))
                 break
+    qvals_mae_mean = np.mean(episodic_qvals_mae)
+    qvals_mae_std = np.std(episodic_qvals_mae)
+    print("MeanQValMAE: {}, StdQValMAE: {}".format(qvals_mae_mean, qvals_mae_std))
 
     # plot averaged returns
     fig, ax = plt.subplots(figsize=(8, 6))
--- loggers_control/scripts/eval_double_homo_dqn_full.py
+++ loggers_control/scripts/eval_double_homo_dqn_full.py
@@ -21,7 +21,7 @@ from agents.dqn import DQNAgent
 if __name__ == "__main__":
     env=DoubleEscapeDiscreteEnv()
     agent = DQNAgent(env=env, name='double_logger_eval')
-    model_path = os.path.join(sys.path[0], 'saved_models/double_escape_discrete/dqn/2020-05-29-17-33/double_logger/models/5093500.h5')
+    model_path = os.path.join(sys.path[0], 'saved_models/double_escape_discrete/dqn/2020-05-29-17-33/double_logger/models/1000000.h5')
     agent.load_model(model_path=model_path)
     agent.epsilon = 0.
     num_episodes = 1000
@@ -30,8 +30,10 @@ if __name__ == "__main__":
     episode_counter = 0
     step_counter = 0
     success_counter = 0
+    episodic_qvals_mae = np.zeros(num_episodes)
     while episode_counter<num_episodes:
         start_time = time.time()
+        qvals_diff = []
         # reset env and get state from it
         obs, rewards, done = env.reset(), [], False
         # next 3 lines generate state, comment out noise if not wanted
@@ -44,6 +46,9 @@ if __name__ == "__main__":
             # take actions, no action will take if deactivated
             act0 = agent.epsilon_greedy(state_0)
             act1 = agent.epsilon_greedy(state_1)
+            qval0 = np.max(agent.qnet_active(np.expand_dims(state_0,axis=0)))
+            qval1 = np.max(agent.qnet_active(np.expand_dims(state_1,axis=0)))
+            qvals_diff.append(np.absolute(qval0-qval1))
             act = np.array([act0, act1])
             # step env
             next_obs, rew, done, info = env.step(act)
@@ -60,14 +65,25 @@ if __name__ == "__main__":
                 success_counter += 1
             rospy.logdebug("\n-\nepisode: {}, step: {} \nstate: {} \naction: {} \nnext_state: {} \nreward: {} \ndone: {} \ninfo: {} \nsucceed: {}\n-\n".format(episode_counter+1, st+1, obs, act, next_obs, rew, done, info, success_counter))
             if done:
-                episode_counter += 1
                 # summarize episode
                 episodic_returns.append(sum(rewards))
-                rospy.loginfo("\n================================================================\nEpisode: {} \nSteps: {} \nEpisodicReturn: {} \nEndState: {} \nTotalSuccess: {} \nTimeConsumed: {} \n================================================================n".format(episode_counter, st+1, episodic_returns[-1], info, success_counter, time.time()-start_time))
+                episodic_qvals_mae[episode_counter] = sum(qvals_diff)/len(qvals_diff)
+                episode_counter += 1
+                rospy.loginfo("\n================================================================\nEpisode: {} \nSteps: {} \nEpisodicReturn: {} \nEndState: {} \nTotalSuccess: {} \nQValueMAE: {} \n================================================================n".format(
+                    episode_counter,
+                    st+1,
+                    episodic_returns[-1], 
+                    info, success_counter, 
+                    sum(qvals_diff)/len(qvals_diff)
+                ))
                 break
+    qvals_mae_mean = np.mean(episodic_qvals_mae)
+    qvals_mae_std = np.std(episodic_qvals_mae)
+    print("MeanQValMAE: {}, StdQValMAE: {}".format(qvals_mae_mean, qvals_mae_std))
 
     # plot averaged returns
     fig, ax = plt.subplots(figsize=(8, 6))
     fig.suptitle('Returns')
     ax.plot(episodic_returns)
     plt.show()
+    
