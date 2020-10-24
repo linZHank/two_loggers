ebefb04d1779ec2195f78e9cc8d8cb9b6da9f900
linzhank <linzhank@gmail.com>
2020-10-24 00:13:56 -0400

    add eval homo, hete_sr, hete_ir, asyn_sr

--- loggers_control/scripts/eval_de_dqn_hete_sr.py
+++ loggers_control/scripts/eval_de_dqn_hete_sr.py
@@ -23,15 +23,18 @@ if __name__=='__main__':
         num_act=num_act
     )
     # load models
-    q_net_path_0 = './saved_models/double_escape_discrete/dqn/hete_ir/2020-10-01-12-56/agent_0/26206'
-    q_net_path_1 = './saved_models/double_escape_discrete/dqn/hete_ir/2020-10-01-12-56/agent_1/26206'
+    q_net_path_0 = './saved_models/double_escape_discrete/dqn/hete_sr/2020-10-06-23-27/agent_0/20000'
+    q_net_path_1 = './saved_models/double_escape_discrete/dqn/hete_sr/2020-10-06-23-27/agent_1/20000'
     agent_0.q.q_net = tf.keras.models.load_model(q_net_path_0)
     agent_1.q.q_net = tf.keras.models.load_model(q_net_path_1)
     agent_0.epsilon = 0
     agent_1.epsilon = 0
-    num_episodes = 10
+    num_episodes = 1000
     ep = 0
     success_counter = 0
+    lead_counter = np.zeros(2)
+    qvals_mae = np.zeros(num_episodes)
+    qvals_diff = []
     o, d, t = env.reset(), False, 0
     while ep < num_episodes: 
         while 'blown' in env.status: 
@@ -40,15 +43,27 @@ if __name__=='__main__':
         s1 = o[[1,0]].flatten()
         a0 = np.squeeze(agent_0.act(np.expand_dims(s0, axis=0)))
         a1 = np.squeeze(agent_1.act(np.expand_dims(s1, axis=0)))
+        qval0 = np.max(agent_0.q.q_net(np.expand_dims(s0,axis=0)))
+        qval1 = np.max(agent_1.q.q_net(np.expand_dims(s1,axis=0)))
+        qvals_diff.append(np.absolute(qval0-qval1))
         n_o, r, d, i = env.step(np.array([int(a0), int(a1)]))
         n_s0 = n_o[[0,1]].flatten()
         n_s1 = n_o[[1,0]].flatten()
         t += 1
         o = n_o.copy()
         if any([d, t==env.max_episode_steps, 'blown' in env.status]):
-            ep +=1
             if i.count('escaped')==2:
                 success_counter += 1
+                if o[0,1] < o[1,1]:
+                    lead_counter[0] += 1
+                else:
+                    lead_counter[1] += 1
+            qvals_mae[ep] = sum(qvals_diff)/len(qvals_diff)
+            ep +=1
+            print("Episode: {}, Succeeded: {}, \nLeader Board: {}".format(ep, success_counter, lead_counter)) 
             o, d, t = env.reset(), False, 0
 
+    qvals_mae_mean = np.mean(qvals_mae)
+    qvals_mae_std = np.std(qvals_mae)
+    print("MeanQValMAE: {}, StdQValMAE: {}".format(qvals_mae_mean, qvals_mae_std))
 
